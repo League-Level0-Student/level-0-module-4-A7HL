185eac0465b38b510f36c6ef4900b180f8ffc6cd
TJHL <tjhl8545@gmail.com>
2022-02-15 19:56:29 -0800

    aaaaaaaa

--- src/_03_char/_2_string_methods/StringMethods.java
+++ src/_03_char/_2_string_methods/StringMethods.java
@@ -5,20 +5,20 @@ public class StringMethods {
 	public static void main(String[] args) {
 		
 		// 1. Create a String variable and initialize it to contain a minimum of 3 characters
-		String uhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh = "idksadfjk";
+		String uhh = "idksadfjk";
 		// 2. Print the 3rd char of your String to the console.
 		//    HINT: .charAt
-		System.out.println(uhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh.charAt(2));
+		System.out.println(uhh.charAt(2));
 		
 		// 3. Print the length of your String to the console.
 		//    HINT: .length()
-		System.out.println(uhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh.length());
+		System.out.println(uhh.length());
 		
 		// 4. Using a for loop, print one char at time to the console.
 		//    HINT: use .length() to determine how many loops
-		int i = uhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh.length();
-		for(i = 0; i <= uhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh.length(); i +=1) {
-			System.out.println(uhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh.charAt(i));
+		int i = uhh.length();
+		for(i = 0; i < uhh.length(); i +=1) {
+			System.out.println(uhh.charAt(i));
 		}
 		
 		// 5. Pick a char inside your String, and use a loop to determine
@@ -26,8 +26,14 @@ public class StringMethods {
 		//    Print the char's position to the console.
 		//    EXAMPLE: if your string is "abc" and you are searching 
 		//             for char 'b', then print "b is at index 1"
-		
+		int a = 0;
+		for(a =0; a < uhh.length(); a+=1) {
+			
+		char index = uhh.charAt(a);
+		if (index==('k')) {
+		System.out.println("k is at index" +index);
+			}
+		}	
 	}
 }
-
 
